dca4c54d1b94d63f5973e590b6d8285a929beecb
Benjamin Wu <benjaminwu2011@gmail.com>
2017-06-11 18:19:32 -0700

    fixed plural/singular grammar error at the end

--- ch07/99BottlesOfBeer.rb
+++ ch07/99BottlesOfBeer.rb
@@ -1,7 +1,8 @@
 startBottles = 99
 newBottles = startBottles
-while newBottles > 0
+while newBottles > 1
   puts newBottles.to_s + " bottles of beer on the wall, " + newBottles.to_s + " bottles of beer.\nTake one down and pass it around, " + (newBottles-1).to_s + " bottles of beer on the wall."
   newBottles -= 1
 end
+puts "1 bottle of beer on the wall, 1 bottle of beer.\nTake one down and pass it around, 0 bottles of beer on the wall."
 puts "No more bottles of beer on the wall, no more bottles of beer.\nGo to the store and buy some more, " + startBottles.to_s + " bottles of beer on the wall."
